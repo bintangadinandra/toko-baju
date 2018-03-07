cd494f9484cd6393dab0179307edee27bfb41088
bintangadinandra <bintangadinandra@gmail.com>
2018-03-08 03:36:55 +0700

    working CMS, considered done, begin populate

--- src/pages/AdminProductList.js
+++ src/pages/AdminProductList.js
@@ -4,16 +4,26 @@ import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import { formatterUtil } from '../utils/formatter'
 import { withRouter, Link } from 'react-router-dom'
+import swal from 'sweetalert'
+import { LoadingSpinner } from '../components/LoadingSpinner';
 
 class componentName extends Component {
   constructor(){
     super()
     this.state = {
-      productList: ''
+      productList: '',
+      isFetching: false
     }
   }
 
   async componentWillMount() {
+    await this.fetchData()
+  }
+
+  async fetchData() {
+    this.setState({
+      isFetching: true
+    })
     try {
       const res = await productService.getAllProduct() 
       console.log('data', res)
@@ -25,10 +35,37 @@ class componentName extends Component {
     } catch (error) {
       console.log(error)
     }
+    this.setState({
+      isFetching: false
+    })
   }
 
   goToCreatePage = () => {
     this.props.history.push('/admin/create')
+  }
+
+  async deleteProduct(id) {
+    try {
+      const willDelete = await swal({
+        title: "Hapus?",
+        text: "Apakah anda yakin ingin menghapus data?",
+        icon: "warning",
+        dangerMode: true,
+      })
+      if (willDelete) {
+        const res = await productService.deleteProduct(id)
+        if (res.data.status==='success') {
+          swal("Success!", "Sukses membuat data baru!", "success")
+          .then(res => {
+            if (res) {
+              this.fetchData()
+            }
+          })
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   render() {
@@ -52,6 +89,7 @@ class componentName extends Component {
         <Link to={`/admin/${props.value}`}>
           <button className="button is-info is-small">View Detail</button>
         </Link>
+        <button className="button is-danger is-small" onClick={() => this.deleteProduct(props.value)}>Hapus</button>
       </div>
     }]
     return (
@@ -63,11 +101,15 @@ class componentName extends Component {
         <hr/>
         <div className="admin_product_list">
           {
-            this.state.productList && 
+            (this.state.productList && !this.state.isFetching) && 
             <ReactTable
               data={this.state.productList}
               columns={columns}
               defaultPageSize={8}></ReactTable>
+          }
+          {
+            (this.state.isFetching) &&
+            <LoadingSpinner condition={true}></LoadingSpinner>
           }
         </div>
       </div>
